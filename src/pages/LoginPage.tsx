975dd7cd158a253bcef2be22df8eb24571fb85df
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-03 22:58:24 +0700

    feat: add dummy auth to dashboard page (no reload)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,26 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './../styles/AuthBox.css';
 
 const LoginPage = () => {
-  return (
-    <div className='auth-container'>
-      <h1>Login</h1>
-      <form className='auth-box'>
-        <div className='input-box-label'>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" required />
+    const nav = useNavigate();
+
+    const handleLogin = () => {
+        nav('/dashboard');
+    };
+    
+    return (
+        <div className='auth-container'>
+            <h1>Login</h1>
+            <form className='auth-box' onSubmit={(e) => {
+                e.preventDefault(); // Menghentikan form dari melakukan reload
+                handleLogin();
+            }}>
+                <div className='input-box-label'>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div className='input-box-label'>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <div className='input-box-submit'>
+                    <button type="submit">Login</button>
+                    <div>Don't have an account?</div>
+                    <Link to="/register">Register</Link>
+                </div>
+            </form>
         </div>
-        <div className='input-box-label'>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required />
-        </div>
-        <div className='input-box-submit'>
-            <button type="submit">Login</button>
-            <div>Don't have an account?</div>
-            <Link to="/register">Register</Link>
-        </div>
-      </form>
-    </div>
   );
 };
 
