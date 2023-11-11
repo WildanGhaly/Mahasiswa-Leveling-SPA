4a06fc8c72b43e38cdbe0fad4c394a5610a6ad3e
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-11 14:45:51 +0700

    feat: add dummy for every pages

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,10 @@
+const SettingsPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Settings Page</h1>
+            <p>Welcome to the Settings Page!</p>
+        </div>
+    );
+}
+
+export default SettingsPage
