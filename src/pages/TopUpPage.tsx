a7aac1d4889e7ea337e69cb0b39db85521df626b
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-12 14:58:44 +0700

    feat: add top up page setup

--- src/pages/TopUpPage.tsx
+++ src/pages/TopUpPage.tsx
@@ -1,21 +1,37 @@
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { Flex, Heading, Text } from "@chakra-ui/react";
+import TopUpOption from "./../components/TopUpOption";
+import CustomAmountTopUp from "./../components/CustomAmountTopUp";
 
 const TopUpPage = () => {
+  const { isLoggedIn } = useAuth();
+  const navigate = useNavigate();
 
-    const { isLoggedIn } = useAuth();
-    const navigate = useNavigate();
+  if (!isLoggedIn) {
+    navigate("/login");
+    return null;
+  }
 
-    if (!isLoggedIn) {
-        navigate('/login');
-    }
+  return (
+    <Flex direction="column" align="center" mt={4}>
+      <Heading as="h1" mb={4}>
+        Top Up Page
+      </Heading>
+      <Text>Welcome to the Top Up Page!</Text>
+      <Flex justify="space-between" mt={4}>
+        {/* Render 6 top-up options */}
+        <TopUpOption imageSrc="public/image/1.jpg" amount={10} />
+        <TopUpOption imageSrc="public/image/1.jpg" amount={20} />
+        <TopUpOption imageSrc="public/image/1.jpg" amount={30} />
+        <TopUpOption imageSrc="public/image/1.jpg" amount={50} />
+        <TopUpOption imageSrc="public/image/1.jpg" amount={100} />
+        <TopUpOption imageSrc="public/image/1.jpg" amount={200} />
+      </Flex>
+      {/* Render the custom amount top-up component */}
+      <CustomAmountTopUp />
+    </Flex>
+  );
+};
 
-    return (
-        <div>
-            <h1>Top Up Page</h1>
-            <p>Welcome to the Top Up Page!</p>
-        </div>
-    );
-}
-
-export default TopUpPage
+export default TopUpPage;
