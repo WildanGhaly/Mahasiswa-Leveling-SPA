d05f9a16965449966cd78e893aef8f2c054dd709
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-12 21:16:08 +0700

    feat: update settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,21 +1,47 @@
+import { Container } from "@chakra-ui/react";
+import PerformanceCard from "../components/PerformanceCard";
+import UserCard from "../components/UserCard";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import ReusableHeader from "../components/ReusableHeader";
 
 const SettingsPage: React.FC = () => {
-
     const { isLoggedIn } = useAuth();
     const navigate = useNavigate();
 
     if (!isLoggedIn) {
         navigate('/login');
     }
-
-    return (
-        <div>
-            <h1>Settings Page</h1>
-            <p>Welcome to the Settings Page!</p>
-        </div>
-    );
-}
-
+      // Dummy data
+      const userData = {
+        username: 'willy',
+        name: 'wildan ghaly',
+        email: 'willy@example.com',
+        points: 120,
+      };
+    
+      const performanceData = {
+        totalAchievement: 5,
+        totalQuest: 10,
+        level: 3,
+        experience: 200,
+      };
+    
+      const handleToggleEmailNotify = () => {
+        // Handle toggle logic
+      };
+    
+      return (
+        <Container maxW="container.lg">
+            <ReusableHeader headingName="Settings" />
+          <UserCard
+            {...userData}
+            onToggleEmailNotify={handleToggleEmailNotify}
+          />
+          <PerformanceCard {...performanceData} />
+        </Container>
+      );
+    };
+    
+    
 export default SettingsPage
