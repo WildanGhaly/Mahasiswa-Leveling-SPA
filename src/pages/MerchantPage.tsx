16734aef3bfd7d07c037ade296d8f2c4bf0f1fb4
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-12 19:35:51 +0700

    feat: update merchant page

--- src/pages/MerchantPage.tsx
+++ src/pages/MerchantPage.tsx
@@ -1,3 +1,7 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import { Container, Flex, Icon, Input, Select, SimpleGrid } from "@chakra-ui/react";
+import MerchCard from "../components/MerchCard";
+import ReusableHeader from "../components/ReusableHeader";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
@@ -11,11 +15,49 @@ const MerchantPage = () => {
     }
 
     return (
-        <div>
-            <h1>Merchant Page</h1>
-            <p>Welcome to the Merchant Page!</p>
-        </div>
+        <Container maxW="container.lg">
+          <ReusableHeader headingName="My Merchant" />
+          <Flex align="center" justify="space-between" py={4}>
+            <Flex mr={4} align="center" w="70%">
+              <Icon as={SearchIcon} mr={2} />
+              <Input placeholder="Search products" />
+            </Flex>
+            <Select placeholder="Select filter" w="20%">
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+            </Select>
+          </Flex>
+          {/* Product Listings */}
+          <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} gap={4}>
+            {/* Product Card Component */}
+            <MerchCard
+              name="Product 1"
+              imageSrc="public/image/1.jpg"
+            />
+            <MerchCard
+              name="Product 2"
+              imageSrc="public/image/2.jpg"
+            />
+            <MerchCard
+              name="Product 3"
+              imageSrc="public/image/3.jpg"
+            />
+            <MerchCard
+              name="Product 4"
+              imageSrc="public/image/4.jpg"
+            />
+            <MerchCard
+              name="Product 5"
+              imageSrc="public/image/5.jpg"
+            />
+            <MerchCard
+              name="Product 6"
+              imageSrc="public/image/6.jpg"
+            />
+            {/* Add more product cards */}
+          </SimpleGrid>
+        </Container>
     );
-}
+};
 
 export default MerchantPage
