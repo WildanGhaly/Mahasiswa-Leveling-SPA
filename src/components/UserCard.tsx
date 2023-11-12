d26a038f38531ebb6ede0ba8a32a542620e67e42
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-12 21:28:39 +0700

    feat: update user card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -5,7 +5,11 @@ import {
     Switch,
     FormControl,
     FormLabel,
+    Avatar,
+    HStack,
+    Icon,
   } from '@chakra-ui/react';
+  import { EmailIcon, StarIcon, AtSignIcon, CheckCircleIcon } from '@chakra-ui/icons';
   
   type UserCardProps = {
     username: string;
@@ -24,20 +28,36 @@ import {
   }) => {
     return (
       <Box borderWidth='1px' borderRadius='lg' p={4} boxShadow='md' marginTop="5vh">
-        <VStack align='stretch'>
-          <Text fontWeight='bold'>{username}</Text>
-          <Text>{name}</Text>
-          <Text>{email}</Text>
-          <Text>{`Points: ${points}`}</Text>
-          <FormControl display='flex' alignItems='center'>
-            <FormLabel htmlFor='email-toggle' mb='0'>
-              Get notify by email
-            </FormLabel>
-            <Switch id='email-toggle' onChange={onToggleEmailNotify} />
-          </FormControl>
-        </VStack>
+        <HStack spacing={4}>
+          <Avatar name={name} src='//path/to/dummy/profile.jpg' size='xl' margin="0 20px" />
+          <VStack align='stretch'>
+            <HStack>
+              <Icon as={StarIcon} color='yellow.400' />
+              <Text fontWeight='bold'>{name}</Text>
+            </HStack>
+            <HStack>
+              <Icon as={AtSignIcon} />
+              <Text>{username}</Text>
+            </HStack>
+            <HStack>
+              <Icon as={EmailIcon} />
+              <Text>{email}</Text>
+            </HStack>
+            <HStack>
+              <Icon as={CheckCircleIcon} />
+              <Text>{`Points: ${points}`}</Text>
+            </HStack>
+            <FormControl display='flex' alignItems='center'>
+              <FormLabel htmlFor='email-toggle' mb='0'>
+                Get notify by email
+              </FormLabel>
+              <Switch id='email-toggle' onChange={onToggleEmailNotify} />
+            </FormControl>
+          </VStack>
+        </HStack>
       </Box>
     );
   };
+  
   export default UserCard;
   
