1f3d3f9bc4ffc05117fcaf65543b0894952c1b1f
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-12 16:26:43 +0700

    feat: add reusable header

---
+++ src/components/ReusableHeader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Flex, Heading, Badge, Spacer } from "@chakra-ui/react";
+
+interface ReusableHeaderProps {
+  headingName: string;
+}
+
+const ReusableHeader: React.FC<ReusableHeaderProps> = ({ headingName }) => {
+  return (
+    <Flex align="center" justify="space-between" py={4}>
+      <Heading>{headingName}</Heading>
+      <Flex align="center">
+        <Spacer />
+        <Badge colorScheme="green" mr={2}>
+          My Points: 500
+        </Badge>
+        <Badge colorScheme="purple">
+          My Money: $100
+        </Badge>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default ReusableHeader;
