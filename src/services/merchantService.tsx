be639bec9c9e2c88ca4cec7187dbc3ccecc54c6e
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-14 06:52:48 +0700

    feat: add https request for merchants

--- src/services/merchantService.tsx
+++ src/services/merchantService.tsx
@@ -4,7 +4,7 @@ const BASE_URL = 'http://localhost:8080';
 
 export const getMerchant = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/merchants`);
+    const response = await axios.get(`${BASE_URL}/merchants`, { withCredentials: true });
     return response.data;
   } catch (error) {
     console.error('Error fetching merchant:', error);
