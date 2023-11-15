668f0ba8fdc780ebaf61f67a6d7fef2770e93e36
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-15 22:01:50 +0700

    feat: add change url to different pagination

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,45 +1,20 @@
-// src/pages/DashboardPage.tsx
-
-import { useAuth } from "../context/AuthContext";
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import {
-  Container,
-  Input,
-  Flex,
-  SimpleGrid,
-  Select,
-  Icon,
-  Box,
-  Button,
-  HStack,
-  
-} from "@chakra-ui/react";
+import { Container, Input, Flex, SimpleGrid, Select, Icon, Box, Button, HStack } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon, SearchIcon } from "@chakra-ui/icons";
 import ProductCard from "../components/cards/ProductCard";
 import ReusableHeader from "../components/layout/ReusableHeader";
 import { getProducts } from "../services/productService";
-import { useEffect, useState } from "react";
 import { Product } from "../types/product";
-
+import { useAuth } from "../context/AuthContext";
 
 const DashboardPage = () => {
   const { isLoggedIn } = useAuth();
   const navigate = useNavigate();
-  const [products, setProducts] = useState<Product[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 10;
-
   const [searchParams, setSearchParams] = useSearchParams();
-
-  const [currentPage, setCurrentPage] = useState(
-    parseInt(searchParams.get('page') || '1', 10)
-  );
-  
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
-    setSearchParams({ page: newPage.toString() });
-  };
-  
+  const [products, setProducts] = useState<Product[]>([]);
+  const currentPage = parseInt(searchParams.get('page') || '1', 10);
+  const totalPages = 10;
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -49,7 +24,11 @@ const DashboardPage = () => {
         setProducts(data);
       });
     }
-  }, [isLoggedIn, navigate, searchParams]);
+  }, [isLoggedIn, navigate]);
+
+  const handlePageChange = (newPage: number) => {
+    setSearchParams({ page: newPage.toString() });
+  };
 
   return (
     <Container maxW="container.lg">
@@ -80,7 +59,7 @@ const DashboardPage = () => {
       <Box py={4}>
         <HStack justify="center">
           <Button
-            onClick={() => setCurrentPage(currentPage - 1)}
+            onClick={() => handlePageChange(Math.max(1, currentPage - 1))}
             disabled={currentPage === 1}
           >
             <ChevronLeftIcon />
@@ -88,21 +67,20 @@ const DashboardPage = () => {
           {[...Array(totalPages).keys()].slice(Math.max(0, currentPage - 5), Math.min(currentPage + 4, totalPages)).map(page => (
             <Button
               key={page}
-              onClick={() => setCurrentPage(page + 1)}
+              onClick={() => handlePageChange(page + 1)}
               variant={page + 1 === currentPage ? "solid" : "ghost"}
             >
               {page + 1}
             </Button>
           ))}
           <Button
-            onClick={() => setCurrentPage(currentPage + 1)}
+            onClick={() => handlePageChange(Math.min(totalPages, currentPage + 1))}
             disabled={currentPage === totalPages}
           >
             <ChevronRightIcon />
           </Button>
         </HStack>
       </Box>
-
     </Container>
   );
 };
