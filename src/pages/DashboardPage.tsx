24eb0a35b007947e32342e842bba7ce79b5af567
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-15 22:41:09 +0700

    feat: add pagination limit

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Container, Input, Flex, SimpleGrid, Select, Icon, Box, Button, HStack } from "@chakra-ui/react";
-import { ChevronLeftIcon, ChevronRightIcon, SearchIcon } from "@chakra-ui/icons";
+import { SearchIcon } from "@chakra-ui/icons";
 import ProductCard from "../components/cards/ProductCard";
 import ReusableHeader from "../components/layout/ReusableHeader";
 import { getProducts } from "../services/productService";
 import { Product } from "../types/product";
 import { useAuth } from "../context/AuthContext";
+import { FaAngleDoubleLeft, FaAngleDoubleRight, FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const DashboardPage = () => {
   const { isLoggedIn } = useAuth();
@@ -56,31 +57,68 @@ const DashboardPage = () => {
           />
         ))}
       </SimpleGrid>
-      <Box py={4}>
+
+      <Box py={4} margin={10}>
         <HStack justify="center">
+          {/* Tombol Halaman Pertama */}
+          <Button
+            onClick={() => handlePageChange(1)}
+            disabled={currentPage === 1}
+            boxShadow={"none"}
+            variant={"ghost"}
+          >
+            <FaAngleDoubleLeft />
+          </Button>
+
+          {/* Tombol Sebelumnya */}
           <Button
             onClick={() => handlePageChange(Math.max(1, currentPage - 1))}
             disabled={currentPage === 1}
+            fontWeight="bold"
+            boxShadow={"none"}
+            variant={"ghost"}
           >
-            <ChevronLeftIcon />
+            <FaAngleLeft />
           </Button>
+
+          {/* Tombol Nomor Halaman */}
           {[...Array(totalPages).keys()].slice(Math.max(0, currentPage - 5), Math.min(currentPage + 4, totalPages)).map(page => (
             <Button
               key={page}
               onClick={() => handlePageChange(page + 1)}
               variant={page + 1 === currentPage ? "solid" : "ghost"}
+              boxShadow={page + 1 === currentPage ? "md" : "none"}
+              fontWeight={"bold"}
+              borderBlock={page + 1 === currentPage ? "1px" : "none"}
+              borderColor={"gray.400"}
             >
               {page + 1}
             </Button>
           ))}
+
+          {/* Tombol Selanjutnya */}
           <Button
             onClick={() => handlePageChange(Math.min(totalPages, currentPage + 1))}
             disabled={currentPage === totalPages}
+            fontWeight="bold"
+            boxShadow={"none"}
+            variant={"ghost"}
           >
-            <ChevronRightIcon />
+            <FaAngleRight />
+          </Button>
+
+          {/* Tombol Halaman Terakhir */}
+          <Button
+            onClick={() => handlePageChange(totalPages)}
+            disabled={currentPage === totalPages}
+            boxShadow={"none"}
+            variant={"ghost"}
+          >
+            <FaAngleDoubleRight />
           </Button>
         </HStack>
       </Box>
+
     </Container>
   );
 };
