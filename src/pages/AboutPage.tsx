6615b144a55b58fa72207b1888a740c545f6c900
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-11-17 04:52:42 +0700

    feat: add home and about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,13 +1,39 @@
 // src/components/pages/AboutPage.tsx
+import { Box, Heading, Text, Image, VStack, Divider } from '@chakra-ui/react';
 
 const AboutPage = () => {
     return (
-      <div>
-        <h1>About Page</h1>
-        <p>This is all about us.</p>
-      </div>
+      <Box p={3}>
+        <VStack spacing={2} align="stretch" >
+          <Box  textAlign="center" py={10} px={6}>
+            <Heading 
+              display="inline-block"
+              as="h1"
+              size="2xl"
+              bgGradient="linear(to-r, teal.400, teal.600)"
+              backgroundClip="text"
+              marginBottom="10px"
+              padding="20px">
+              About Page
+            </Heading>
+            <Divider />
+            <Text fontSize="lg" mt={3} w="60%" textAlign="center" mx="auto">
+              At our website, you have the opportunity to turn your points earned from Mahasiswa Leveling into fantastic merchandise. It's a seamless process - simply register by entering the token provided in your Mahasiswa Leveling user profile. Once registered, you can top up your account, explore our wide range of merchandise, and even track your purchase history.
+              We're here to make your online shopping experience both rewarding and enjoyable. Feel free to explore our selection of merchandise and discover the convenience of converting your hard-earned points into tangible items.
+            </Text>
+          </Box>
+          <Box>
+            <Image 
+              borderRadius='full'
+              boxSize='150px'
+              src='public/image/1.jpg' 
+              alt='Company Logo'
+              m='auto'
+            />
+          </Box>
+        </VStack>
+      </Box>
     );
   };
   
-  export default AboutPage;
-  
+export default AboutPage;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,54 @@
 // src/components/modals/HomePage.tsx
 
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
 const HomePage = () => {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useAuth();
+
+  const handleGetStarted = () => {
+    if (isLoggedIn) {
+      navigate('/dashboard');
+    } else {
+      navigate('/login');
+    }
+  };
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
-    </div>
+    <Box textAlign="center" py={10} px={6}>
+      <Heading
+        display="inline-block"
+        as="h1"
+        size="2xl"
+        bgGradient="linear(to-r, teal.400, teal.600)"
+        backgroundClip="text"
+        marginBottom="10px"
+        padding="20px">
+        Home Page
+      </Heading>
+      <Text fontSize="18px" mt={3} mb={2}>
+        Welcome to the home page!
+      </Text>
+      <Text color={'gray.500'}>
+        Discover our amazing application with lots of features and capabilities.
+      </Text>
+      <Button
+        colorScheme="teal"
+        bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+        color="white"
+        variant="solid"
+        mt={3}
+        mb={3}
+        _hover={{
+          bgGradient: "linear(to-r, teal.600, teal.700, teal.800)",
+        }}
+        onClick={handleGetStarted}
+        >
+        Get Started
+      </Button>
+    </Box>
   );
 };
 
